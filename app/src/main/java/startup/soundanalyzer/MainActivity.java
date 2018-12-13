2401ea2ca1a77720e09fb6df2915ff93e881f5b7
felixgaggl <felix.gaggl@outlook.com>
2018-12-13 13:07:15 +0100

    Initial Commit

---
+++ app/src/main/java/startup/soundanalyzer/MainActivity.java
@@ -0,0 +1,13 @@
+package startup.soundanalyzer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
